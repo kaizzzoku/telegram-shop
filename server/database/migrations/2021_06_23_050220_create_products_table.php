f05d4cfe40a96289df71c1c9ccfb22bf4c96bc03
kaizzzoku <z.zzzoku.z@gmail.com>
2021-06-23 10:18:26 +0300

    Create command for products parsing

--- server/database/migrations/2021_06_23_050220_create_products_table.php
+++ server/database/migrations/2021_06_23_050220_create_products_table.php
@@ -16,7 +16,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->bigInteger('qty');
+            $table->unsignedBigInteger('quantity');
             $table->text('description');
             $table->decimal('price',15, 2);
             $table->string('image');
